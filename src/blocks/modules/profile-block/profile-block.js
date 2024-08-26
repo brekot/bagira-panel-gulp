203780e0abbaf11331b82be1b64358d523022f94
Andrey Brekotkin <brekot@ya.ru>
2024-08-26 17:37:52 +0300

    Check password

--- src/blocks/modules/profile-block/profile-block.js
+++ src/blocks/modules/profile-block/profile-block.js
@@ -1,36 +1,3 @@
-document.addEventListener('mousedown', (e) => {
-
-    let elem = e.target.closest('.form-profile__password-toggle');
-
-    if (elem)
-    {
-        let input = elem.previousElementSibling;
-
-        if (input.type === "password")
-        {
-            input.type = "text";
-
-            elem.classList.add('form-profile__password-toggle_show');
-        }
-        else
-        {
-            input.type = "password";
-
-            elem.classList.remove('form-profile__password-toggle_show');
-        }
-    }
-});
-
-document.addEventListener('mouseup', (e) => {
-
-    let elem = e.target.closest('.form-profile__password-toggle');
-
-    if (elem)
-    {
-        elem.previousElementSibling.focus();
-    }
-});
-
 document.querySelector('.readonly-toggle').addEventListener('click', (e) => {
 
     let elem = e.target,
@@ -52,4 +19,45 @@ document.querySelector('.readonly-toggle').addEventListener('click', (e) => {
     elem.classList.toggle('btn_dark');
 
     sendBtn.classList.toggle('d-none');
+});
+
+document.querySelectorAll('.form-profile__input_password').forEach((e) => {
+
+    e.addEventListener('focus', (e) => {
+
+        e.target.closest('.form-profile__line').classList.add('form-profile__line_focus');
+    });
+});
+
+document.addEventListener('click', (e) => {
+
+    let elem = e.target.closest('.form-profile__line_focus');
+
+    document.querySelectorAll('.form-profile__line_focus').forEach((e) => {
+
+        if (elem !== e)
+        {
+            e.classList.remove('form-profile__line_focus')
+        }
+    });
+
+    elem = e.target.closest('.form-profile__password-toggle');
+
+    if (elem)
+    {
+        let input = elem.previousElementSibling;
+
+        if (input.type === "password")
+        {
+            input.type = "text";
+
+            elem.classList.add('form-profile__password-toggle_show');
+        }
+        else
+        {
+            input.type = "password";
+
+            elem.classList.remove('form-profile__password-toggle_show');
+        }
+    }
 });
